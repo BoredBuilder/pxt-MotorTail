0e3d6a4c0e16773e37de38167243c5c651b42f5b
BoredBuilder <152805324+BoredBuilder@users.noreply.github.com>
2025-11-30 02:51:53 -0500

    test

--- test.ts
+++ test.ts
@@ -1,1 +1,19 @@
-// tests go here; this will not be compiled when this package is used as an extension.
+// tests go here; this will not be compiled when this package is used as a library
+
+//Control motor A forward and set speed
+MotorTail.MotorRun(Motor.dc1, Dir.forward, 0)
+
+//Stop motor
+MotorTail.MotorStop(Motor.dc2)
+
+//Turn the steering gear S0 to the starting position
+MotorTail.ServosTurnZero(Servo.S0)
+
+//Turn the steering gear S0 to the maximum position
+MotorTail.ServosTurnFull(Servo.S0)
+
+//Stop Servo
+MotorTail.ServoStop(Servo.S0)
+
+//Servo Turn Angle
+MotorTail.ServoTurnAngle(Servo.S1, 180)
